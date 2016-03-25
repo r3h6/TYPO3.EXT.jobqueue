fa2ba7aedbaea385298dbf8597b7e588aabd22bc
r3h6 <r3h6@outlook.com>
2016-03-25 20:38:40 +0100

    [TASK] Testing before execute daemon

--- Classes/Command/JobCommandController.php
+++ Classes/Command/JobCommandController.php
@@ -45,38 +45,53 @@ class JobCommandController extends CommandController
     /**
      * Experimental command
      *
-     * @param  int  $id       [description]
+     * @param  int  $id       Daemon id
      * @param  string  $queueName [description]
      * @param  integer $timeout   [description]
-     * @return [type]             [description]
      */
     public function daemonCommand($id, $queueName, $timeout = 0)
     {
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
             throw new \BadFunctionCallException("Command 'job:daemon' is not available on windows systems", 1458844709);
         }
-        if (!is_callable('exec')) {
-            throw new \BadFunctionCallException('Function "exec" is not callable', 1458849505);
-        }
-        if (!is_callable('proc_open')) {
-            throw new \BadFunctionCallException('Function "proc_open" is not callable', 1458849578);
-        }
-        if (!is_callable('proc_get_status')) {
-            throw new \BadFunctionCallException('Function "proc_get_status" is not callable', 1458849585);
-        }
 
+        $cliDispatchPath = PATH_site . 'typo3/cli_dispatch.phpsh';
+
+        // Check if daemon is already running.
         $status = $this->registry->get('daemon:' . $id);
         if (is_array($status) && isset($status['pid'])) {
-            exec(sprintf("ps -p %s", $status['pid']), $output);
-            if (count($output) > 1) {
+            if ($this->processExist($status['pid'])) {
                 $this->outputFormatted('Daemon "%s" is already running in process "%s".', [$id, $status['pid']]);
                 return;
             }
         }
 
-        $cliDispatchPath = PATH_site . 'typo3/cli_dispatch.phpsh';
-        $command = "exec php $cliDispatchPath extbase job:work --queue-name=\"$queueName\" --timeout=\"$timeout\" --daemon";
+        // Test if a process can be started and the system gets the right pids.
+        $command = "exec php $cliDispatchPath extbase job:test --id=\"$id\"";
+        $test = $this->processOpen($command);
+        if ($test['pid'] == getmypid()) {
+            throw new \Exception("Daemon not started because pid's are same", 1458897118);
+        }
+        $i = 0;
+        while ($this->registry->get('pid:' . $id) != $test['pid']) {
+            if (++$i > 10) {
+                throw new \Exception("Daemon not started because the system failded to verify the pid's", 1458894146);
+            }
+            sleep(1);
+        }
+        if (!$this->processExist($test['pid'])) {
+            throw new \Exception("Daemon not started because the system failded to verify the test process", 1458896762);
+        }
 
+        // Open daemon process
+        $command = "exec php $cliDispatchPath extbase job:work --queue-name=\"$queueName\" --timeout=\"$timeout\" --daemon";
+        $status = $this->processOpen($command);
+        $this->registry->set('daemon:' . $id, $status);
+        $this->outputFormatted('Daemon "%s" started in a new process "%s".', [$id, $status['pid']]);
+    }
+
+    protected function processOpen($command)
+    {
         $pipes = [];
         $descriptorspec = [];
         $process = proc_open($command, $descriptorspec, $pipes);
@@ -88,16 +103,37 @@ class JobCommandController extends CommandController
             throw new \RuntimeException('Could not get status!', 1458849124);
 
         }
-
-        $this->registry->set('daemon:' . $id, $status);
-
-        $this->outputFormatted('Daemon "%s" started in a new process "%s".', [$id, $status['pid']]);
+        return $status;
     }
 
+    protected function processExist($pid)
+    {
+        exec(sprintf("ps -p %s", $pid), $output);
+        return (count($output) > 1);
+    }
+
+    /**
+     * Test (Internal command only)
+     * @param int $id Daemon id
+     * @cli
+     */
+    public function testCommand($id)
+    {
+        $pid = getmypid();
+        $key = 'pid:' . $id;
+        $this->registry->set($key, $pid);
+        // $this->outputFormatted('Registred key "%s" with value "%s"', [$key, $pid]);
+        sleep(10);
+    }
+
+    /**
+     * Sends signal for stop all running daemon processes.
+     * @cli
+     */
     public function killCommand()
     {
         $this->registry->set(Registry::DAEMON_RESTART_KEY, time());
-        $this->outputLine('Broadcast kill signal');
+        $this->outputLine('Broadcast signal');
     }
 
     /**
