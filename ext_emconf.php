89724979be046c61ff6378e4c56820a86b8668a1
r3h6 <r3h6@outlook.com>
2017-04-08 08:57:33 +0200

    [TASK] Emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.1',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-7.6.99',
+			'typo3' => '7.6.0-8.99.99',
 		),
 		'conflicts' => array(
 		),
